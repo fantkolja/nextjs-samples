9e7d8140f40b42ce4ffbe4df5f12f21bc8e68aa8
Mykola Fant <mykola.fant@hazelcast.com>
2024-01-29 18:21:03 +0200

    add docs loading

--- app-router/src/app/page.tsx
+++ app-router/src/app/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
 import { Header } from '@/ui/components/common/Header/Header';
 import { Menu } from '@/ui/components/common/Menu/Menu';
 import { Logo } from '@/ui/components/common/Logo/Logo';
-import { clsx } from 'clsx';
 import React from 'react';
 
 export default function Home() {
