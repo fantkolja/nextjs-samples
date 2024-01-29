708cbdff26a80cf36e829e03c63609469b7cc54b
Mykola Fant <mykola.fant@hazelcast.com>
2024-01-29 20:01:11 +0200

    merge main

--- app-router/src/app/(non-home-pages)/layout.tsx
+++ app-router/src/app/(non-home-pages)/layout.tsx
@@ -1,7 +1,8 @@
 import { Header } from '@/ui/components/common/Header/Header';
 import { Menu } from '@/ui/components/common/Menu/Menu';
+import { LayoutPageProps } from '@/types/page';
 
-export default function NonHomePageLayout({ children }) {
+export default function NonHomePageLayout({ children }: LayoutPageProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Menu />
--- app-router/src/app/layout.tsx
+++ app-router/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { LayoutPageProps } from '@/types/page';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,9 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: LayoutPageProps) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
