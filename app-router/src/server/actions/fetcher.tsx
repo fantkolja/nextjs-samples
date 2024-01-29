b82c59698b7d6f936ecdbd34c2ef84ae5b3ee861
Mykola Fant <mykola.fant@hazelcast.com>
2024-01-29 18:46:06 +0200

    add docs loading

--- app-router/src/server/actions/fetcher.tsx
+++ app-router/src/server/actions/fetcher.tsx
@@ -1,6 +1,11 @@
 import { Post } from '@/types/posts';
 
+export const sleep = async (timeout: number) => {
+  return new Promise(res => setTimeout(res, timeout));
+}
+
 export const fetchPosts = async (): Promise<Post[]> => {
+  await sleep(3000);
   return fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json());
 }
