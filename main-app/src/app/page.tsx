eb7b2f2426531520d3a3608e6278872142634c0d
Mykola Fant <mykola.fant@hazelcast.com>
2024-03-16 12:51:45 +0200

    Revert "add db connection"
    This reverts commit c1403bc2917ca44c1749d1803c9851785359bc57.

--- main-app/src/app/page.tsx
+++ main-app/src/app/page.tsx
@@ -2,16 +2,12 @@ import { Header } from '@/ui/components/common/Header/Header';
 import { Menu } from '@/ui/components/common/Menu/Menu';
 import { Logo } from '@/ui/components/common/Logo/Logo';
 import React from 'react';
-import { getCustomers } from '@/server/data/db';
 
-export default async function Home() {
-  const customers = await getCustomers();
-  console.log(customers);
+export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header/>
       <Logo/>
-
       <Menu/>
     </main>
   );
