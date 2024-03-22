31a6ad7a2e218d20ce826506a85115bf8f0d50aa
Mykola Fant <mykola.fant@hazelcast.com>
2024-03-22 19:00:24 +0200

    add dynamic

--- main-app/src/app/(non-home-pages)/deploy/page.tsx
+++ main-app/src/app/(non-home-pages)/deploy/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = 'force-dynamic';
 export default async function Deploy() {
   const customers = await getCustomers();
   return (
-    <div>
+    <div className="flex flex-col items-center gap-2">
       <p>{process.env.ENV_SERVER_WELCOME}</p>
       <h2 className="text-4xl">Deploy</h2>
       <p>You have {customers.length} customers</p>
