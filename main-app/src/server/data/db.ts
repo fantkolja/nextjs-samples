e4b94e088d82bc38bc1cd1549dca86c71d26bc4b
Mykola Fant <mykola.fant@hazelcast.com>
2024-03-23 10:17:30 +0200

    add

--- main-app/src/server/data/db.ts
+++ main-app/src/server/data/db.ts
@@ -1,10 +1,10 @@
 import { sql } from '@vercel/postgres';
+import { Customer } from '@/types/customer';
 
 export const getCustomers = async () => {
   console.log('[DB] Query Customers');
-  // const data = await pool.sql<any>`SELECT * FROM customers`;
-  const data = await sql<any>`SELECT * FROM customers`;
-
+  const data = await sql<Customer>`SELECT * FROM customers`;
+  console.log('[DB] Received Customers', data.rows.length);
   return data.rows;
 };
 
