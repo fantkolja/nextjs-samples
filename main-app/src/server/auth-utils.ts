659895cb5f244d9ab0874d332073b4a861380fbd
Mykola Fant <mykola.fant@hazelcast.com>
2024-04-11 19:52:10 +0300

    add Google and Github

--- main-app/src/server/auth-utils.ts
+++ main-app/src/server/auth-utils.ts
@@ -5,67 +5,99 @@ import { User } from '@/types/user';
 import { getUserByEmail } from '@/server/data/db';
 import bcrypt from 'bcryptjs';
 
-const SESSION_COOKIE_NAME = 'session';
-const SESSION_DURATION_IN_SECONDS = 10;
-const SESSION_DURATION = SESSION_DURATION_IN_SECONDS * 1000;
-const jwtSecret = new TextEncoder().encode(process.env.JWT_SECRET);
+// const SESSION_COOKIE_NAME = 'session';
+// const SESSION_DURATION_IN_SECONDS = 10;
+// const SESSION_DURATION = SESSION_DURATION_IN_SECONDS * 1000;
+// const jwtSecret = new TextEncoder().encode(process.env.AUTH_SECRET);
+//
+// export async function encrypt(payload: { user: User, expires: Date }) {
+//   return await new SignJWT(payload)
+//     .setProtectedHeader({ alg: 'HS256' })
+//     .setIssuedAt()
+//     .setExpirationTime(`${SESSION_DURATION_IN_SECONDS} sec from now`)
+//     .sign(jwtSecret);
+// }
+//
+// export async function decrypt(input: string): Promise<any> {
+//   const { payload } = await jwtVerify(input, jwtSecret, {
+//     algorithms: ["HS256"],
+//   });
+//   return payload;
+// }
+//
+// export async function login(email: string, password: string) {
+//   let isLoggedIn = false;
+//
+//   const user = await getUserByEmail(email);
+//
+//   if (user) {
+//     let doesPasswordMatch = await bcrypt.compare(password, user.password);
+//
+//     if (doesPasswordMatch) {
+//       const expires = new Date(Date.now() + SESSION_DURATION);
+//       const session = await encrypt({ user: user as unknown as User, expires });
+//
+//       cookies().set(SESSION_COOKIE_NAME, session, { expires, httpOnly: true });
+//       isLoggedIn = true;
+//     }
+//   }
+//   return isLoggedIn;
+// }
+//
+// export async function logout() {
+//   cookies().set(SESSION_COOKIE_NAME, "", { expires: new Date(0) });
+// }
+//
+// export async function getSession() {
+//   const session = cookies().get(SESSION_COOKIE_NAME)?.value;
+//   if (!session) return null;
+//   return await decrypt(session);
+// }
+//
+// export async function refreshSession(request: NextRequest) {
+//   const session = request.cookies.get(SESSION_COOKIE_NAME)?.value;
+//   if (!session) return;
+//
+//   const parsed = await decrypt(session);
+//   parsed.expires = new Date(Date.now() + SESSION_DURATION);
+//   const res = NextResponse.next();
+//   res.cookies.set({
+//     name: SESSION_COOKIE_NAME,
+//     value: await encrypt(parsed),
+//     httpOnly: true,
+//     expires: parsed.expires,
+//   });
+//   return res;
+// }
 
-export async function encrypt(payload: { user: User, expires: Date }) {
-  return await new SignJWT(payload)
-    .setProtectedHeader({ alg: 'HS256' })
-    .setIssuedAt()
-    .setExpirationTime(`${SESSION_DURATION_IN_SECONDS} sec from now`)
-    .sign(jwtSecret);
-}
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import NextAuth, { NextAuthConfig } from 'next-auth';
 
-export async function decrypt(input: string): Promise<any> {
-  const { payload } = await jwtVerify(input, jwtSecret, {
-    algorithms: ["HS256"],
-  });
-  return payload;
-}
-
-export async function login(email: string, password: string) {
-  let isLoggedIn = false;
-
-  const user = await getUserByEmail(email);
-
-  if (user) {
-    let doesPasswordMatch = await bcrypt.compare(password, user.password);
-
-    if (doesPasswordMatch) {
-      const expires = new Date(Date.now() + SESSION_DURATION);
-      const session = await encrypt({ user: user as unknown as User, expires });
-
-      cookies().set(SESSION_COOKIE_NAME, session, { expires, httpOnly: true });
-      isLoggedIn = true;
-    }
+export const nextAuth: NextAuthConfig = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account?.provider === 'google') {
+        return !!profile?.email_verified;
+      }
+      return true;
+    },
   }
-  return isLoggedIn;
 }
 
-export async function logout() {
-  cookies().set(SESSION_COOKIE_NAME, "", { expires: new Date(0) });
-}
-
-export async function getSession() {
-  const session = cookies().get(SESSION_COOKIE_NAME)?.value;
-  if (!session) return null;
-  return await decrypt(session);
-}
-
-export async function refreshSession(request: NextRequest) {
-  const session = request.cookies.get(SESSION_COOKIE_NAME)?.value;
-  if (!session) return;
-
-  const parsed = await decrypt(session);
-  parsed.expires = new Date(Date.now() + SESSION_DURATION);
-  const res = NextResponse.next();
-  res.cookies.set({
-    name: SESSION_COOKIE_NAME,
-    value: await encrypt(parsed),
-    httpOnly: true,
-    expires: parsed.expires,
-  });
-  return res;
-}
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth(nextAuth);
--- main-app/src/ui/components/auth/LoginForm.tsx
+++ main-app/src/ui/components/auth/LoginForm.tsx
@@ -1,11 +1,7 @@
-import {
-  AtSymbolIcon,
-  KeyIcon,
-} from '@heroicons/react/24/outline';
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/ui/components/common/Button/Button';
-import { login } from '@/server/auth-utils';
-import { redirect } from 'next/navigation';
+import { signIn } from '@/server/auth-utils';
+import { AtSymbolIcon, KeyIcon } from '@heroicons/react/24/outline';
+import { ArrowRightIcon } from '@heroicons/react/20/solid';
 
 export interface LoginFormData {
   email: string;
@@ -15,66 +11,99 @@ export interface LoginFormData {
 export default function LoginForm() {
   const onSubmit = async (formData: FormData) => {
     'use server'
-    await login(
-      formData.get('email') as string,
-      formData.get('password') as string,
-    );
-    redirect('/');
+    await signIn('credentials');
+    // await login(
+    //   formData.get('email') as string,
+    //   formData.get('password') as string,
+    // );
+    // redirect('/');
   };
   return (
-    <form className="space-y-3 w-full" action={onSubmit}>
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8 text-gray-900">
-        <h1 className={`mb-3 text-2xl text-black text-center`}>
-          Sign in
-        </h1>
-        <div className="w-full">
-          <div>
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
-                required
-              />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    <div>
+      <form className="space-y-3 w-full" action={onSubmit}>
+        <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8 text-gray-900">
+          <h1 className="mb-3 text-2xl text-black text-center">
+            Sign in
+          </h1>
+          <div className="w-full">
+            <div>
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                  required
+                />
+                <AtSymbolIcon
+                  className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+              </div>
+            </div>
+            <div className="mt-4">
+              <label
+                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  required
+                  minLength={6}
+                />
+                <KeyIcon
+                  className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
+              </div>
             </div>
           </div>
-          <div className="mt-4">
-            <label
-              className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="relative">
-              <input
-                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="password"
-                type="password"
-                name="password"
-                placeholder="Enter password"
-                required
-                minLength={6}
-              />
-              <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
+          <Button className="mt-4 w-full bg-blue-700">
+            Sign in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
+          </Button>
+          <div className="flex h-8 items-end space-x-1">
+            {/* Add form errors here */}
           </div>
         </div>
-        <Button className="mt-4 w-full bg-blue-700">
-          Sign in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
-        <div className="flex h-8 items-end space-x-1">
-          {/* Add form errors here */}
-        </div>
+      </form>
+      <div className="flex gap-4 justify-between">
+        <form
+          className="w-1/2"
+          action={async () => {
+            'use server';
+            await signIn('github');
+          }}
+        >
+          <Button
+            className="mt-4 w-full !bg-white !text-black !justify-center"
+          >
+            GitHub
+          </Button>
+        </form>
+        <form
+          className="w-1/2"
+          action={async () => {
+            'use server';
+            await signIn('google');
+          }}
+        >
+          <Button
+            className="mt-4 w-full !bg-white !text-black !justify-center"
+          >
+            Google
+          </Button>
+        </form>
       </div>
-    </form>
+    </div>
   );
 }
