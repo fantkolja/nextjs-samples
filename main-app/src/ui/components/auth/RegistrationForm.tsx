bc937532057562d047a0eb7f97c4abc648bc2904
Mykola Fant <mykola.fant@hazelcast.com>
2024-04-18 10:10:10 +0300

    fix

--- main-app/src/ui/components/auth/RegistrationForm.tsx
+++ main-app/src/ui/components/auth/RegistrationForm.tsx
@@ -5,7 +5,6 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/ui/components/common/Button/Button';
-import { login } from '@/server/auth-utils';
 import { redirect } from 'next/navigation';
 import { createUser } from '@/server/data/db';
 
