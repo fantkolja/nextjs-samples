e0acf07cc141661eb28aef78de6e0afbd38dcf39
Mykola Fant <mykola.fant@hazelcast.com>
2025-03-14 11:24:05 +0200

    fix global playbook loader for prod build

---
+++ samples/crud-api-sample/customers/[id]/route.ts
@@ -0,0 +1,24 @@
+import { createCustomer, deleteCustomer, getCustomers, updateCustomer } from '@/server/data/db';
+import { CustomerPayload } from '@/types/customer';
+import { NextRequest } from 'next/server';
+
+export interface PageParams {
+  id: string;
+}
+
+export async function DELETE(_req: NextRequest, props: { params: Promise<PageParams> }) {
+  const params = await props.params;
+  // @TODO: add validation stage
+  const result = await deleteCustomer(params.id);
+
+  return Response.json({ count: result.rowCount });
+}
+
+export async function PATCH(request: NextRequest, props: { params: Promise<PageParams> }) {
+  const params = await props.params;
+  // @TODO: add validation stage
+  const updatedFields = await request.json();
+  const result = await updateCustomer(params.id, updatedFields as Partial<CustomerPayload>);
+
+  return Response.json({ count: result.rowCount });
+}
