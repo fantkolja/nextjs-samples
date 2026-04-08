0e3ac9f709cee9f080c53fceeeb8ff90f7d17c65
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2026-04-08 16:29:08 +0300

    vercel

--- src/app/api/articles/route.ts
+++ src/app/api/articles/route.ts
@@ -4,7 +4,8 @@ export async function GET() {
   const articles = await fetchAllPosts();
 
   return Response.json({
-    message: process.env.ENV_WELCOME,
-    articles,
+    server_welcome: process.env.ENV_WELCOME,
+    public_welcome: process.env.NEXT_PUBLIC_HEADER_WELCOME,
+    articles: 'articles',
   });
 }
